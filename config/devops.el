ff65f946500978eb225c81c1509739c55789b4a3
aplacaba <alacaba@fastmail.com>
2025-08-06 23:35:07 +0800

    Added devops packages

--- config/devops.el
+++ config/devops.el
@@ -1,3 +1,24 @@
-;;; Devops config packages
+;;; devops.el --- Devops configuration
+;;; Commentary:
+;;; Code:
+
+;; terraform
+(use-package terraform-mode
+  :init
+  (setopt terraform-format-on-save t)
+  (defun my-terraform-mode-init ()
+    ;; if you want to use outline-minor-mode
+    (outline-minor-mode 1))
+  :hook
+  (terraform-mode-hook . my-terraform-mode-init))
+
+;; docker
+(use-package docker
+  :bind ("C-c d" . docker))
+
+;; cloudformation
+(use-package cfn-mode)
+
 
 (provide 'devops)
+;;; devops.el ends here
