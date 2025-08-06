3a885c48c565b7a4091af6ba9905b1a1596f538b
aplacaba <alacaba@fastmail.com>
2025-08-07 01:29:37 +0800

    Add sbcl config

--- config/llisp.el
+++ config/llisp.el
@@ -1,1 +1,18 @@
+;;; llisp.el --- Lisp Config
+;;; Commentary:
+;;; Code:
+
+;; commonlisp tooling
+(load (expand-file-name "~/.roswell/helper.el"))
+(setq
+  inferior-lisp-program ""
+  slime-lisp-implementations '((sbcl ("ros" "-Q" "run") :coding-system utf-8-unix)
+			       (qlot ("qlot" "exec" "sbcl") :coding-system utf-8-unix)))
+
+(defun g-slime-start ()
+  (interactive)
+  (let ((current-prefix-arg '-))
+    (call-interactively 'slime)))
+
 (provide 'llisp)
+;;; llisp.el ends here
