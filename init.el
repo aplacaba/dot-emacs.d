d5c79583b0584231fd444ff4be85404424d144f7
aplacaba <alacaba@fastmail.com>
2026-04-23 14:38:15 +0800

    refactor(init): remove unnecessary progn wrapper around requires

--- init.el
+++ init.el
@@ -20,21 +20,20 @@
 (add-to-list 'load-path my/emacs-source-path)
 
 ;; load config
-(progn
-  (require 'core)        ;; emacs
-  (require 'rice)        ;; looks
-  (require 'nav)         ;; navigation
-  (require 'notes)       ;; note taking
-  (require 'devops)      ;; devops config
-  (require 'programming) ;; programming
-  (require 'llisp)       ;; lisp config
-  (require 'bindings)    ;; key bindings
+(require 'core)        ;; emacs
+(require 'rice)        ;; looks
+(require 'nav)         ;; navigation
+(require 'notes)       ;; note taking
+(require 'devops)      ;; devops config
+(require 'programming) ;; programming
+(require 'llisp)       ;; lisp config
+(require 'bindings)    ;; key bindings
 
-  (when is-windows
-    (require 'windows))
+(when is-windows
+  (require 'windows))
 
-  (when is-mac
-    (require 'mac)))
+(when is-mac
+  (require 'mac))
 
 ;; load custom file (set in core.el)
 (load custom-file 'noerror 'nomessage)
